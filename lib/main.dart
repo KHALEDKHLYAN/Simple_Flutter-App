717126549d1715142e38699074656b477d6265ac
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-07-13 07:45:32 +0300

    debug addition

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(const MaterialApp(
+      debugShowMaterialGrid: false,
       home: NinjaCard(),
     ));
 
